e7dd485b1988895166e09fdaf5b0262ff4165a67
SurajMahamuni15 <128219758+SurajMahamuni15@users.noreply.github.com>
2023-09-21 11:45:37 +0530

    Profile Setting adapter changes

--- app/src/main/java/com/phntechnolab/sales/fragment/ProfileFragment.kt
+++ app/src/main/java/com/phntechnolab/sales/fragment/ProfileFragment.kt
@@ -6,12 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.phntechnolab.sales.R
+import com.phntechnolab.sales.adapter.ProfileSettingAdapter
 import com.phntechnolab.sales.databinding.FragmentProfileBinding
+import com.phntechnolab.sales.model.SettingModel
 
 
 class ProfileFragment : Fragment() {
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
+
+    private var _adapter : ProfileSettingAdapter? = null
+    private val adapter get() = _adapter!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -22,11 +28,30 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        initializeAdapter()
+
         return binding.root
+    }
+
+    private fun initializeAdapter() {
+        _adapter = ProfileSettingAdapter()
+        binding.settingRv.adapter = adapter
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setDataToAdapter()
+    }
+
+    private fun setDataToAdapter() {
+        adapter.setData(ArrayList<SettingModel>().apply {
+            add(SettingModel(R.drawable.my_account,resources.getString(R.string.my_account),""))
+            add(SettingModel(R.drawable.my_account,resources.getString(R.string.my_account),""))
+            add(SettingModel(R.drawable.my_account,resources.getString(R.string.my_account),""))
+            add(SettingModel(R.drawable.my_account,resources.getString(R.string.my_account),""))
+            add(SettingModel(R.drawable.my_account,resources.getString(R.string.my_account),""))
+        })
     }
 
     override fun onDestroy() {
