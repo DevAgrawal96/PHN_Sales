ebd64edfa221b8d6beb244c9d88cd9c68570809d
Dev Agrawal <dev.agrawal@phntechnology.com>
2023-09-25 18:59:22 +0530

    Done with edit and add all the three stages data

--- app/src/main/java/com/phntechnolab/sales/fragment/AddSchoolFragment.kt
+++ app/src/main/java/com/phntechnolab/sales/fragment/AddSchoolFragment.kt
@@ -1,5 +1,7 @@
 package com.phntechnolab.sales.fragment
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.os.Message
 import android.view.LayoutInflater
@@ -26,6 +28,7 @@ import com.phntechnolab.sales.util.TextValidator
 import com.phntechnolab.sales.viewmodel.AddSchoolViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
+import java.util.Calendar
 import java.util.regex.Pattern
 
 @AndroidEntryPoint
@@ -110,12 +113,6 @@ class AddSchoolFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        if (viewModel.newSchoolData.value != null) {
-            setSchoolDetails()
-        }
-
-        setDropdowns()
 
         oncClickListener()
 
@@ -207,9 +204,9 @@ class AddSchoolFragment : Fragment() {
 
                 } else if (type == "checkIntegerNullOrZero") {
                     val isNameEmpty: Boolean =
-                        editText.text.toString().isNullOrEmpty() || editText.text.toString() == "0";
+                        editText.text.toString().isNullOrEmpty() || editText.text.toString() == "0"
                     if (isNameEmpty)
-                        til.error = errorMessage
+//                        til.error = errorMessage
                     else
                         til.error = null
                 } else if (type == "phone" || type == "email") {
@@ -218,17 +215,17 @@ class AddSchoolFragment : Fragment() {
 
                     when (type) {
                         "phone" -> {
-                            pattern = Pattern.compile("^[0123456789]{15}\$")
+                            pattern = Pattern.compile("[0123456789]{10,15}")
                         }
                         "email" -> {
-                            pattern = Pattern.compile("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9]+[.-][a-zA-Z][a-z.A-Z]+\$")
+                            pattern = Pattern.compile("[a-zA-Z0-9+_.-]+@[a-zA-Z0-9]+[.-][a-zA-Z][a-z.A-Z]+")
                         }
                         else -> {
 
                         }
                     }
 
-                    if (pattern.matcher(text).matches())
+                    if (!pattern.matcher(text).matches())
                         til.error = errorMessage
                     else
                         til.error = null
@@ -237,9 +234,23 @@ class AddSchoolFragment : Fragment() {
         })
     }
 
-    private fun setDropdowns() {
+    private fun setDropdowns(_schoolData: SchoolData?) {
+        //Set board spinner data
+
         val dropdown: AutoCompleteTextView = binding.basicDetails.boardSpinner
-        val items = arrayOf("State Board", "CBSE", "ICSE", "NIOS", "IB", "CIE")
+        val items = ArrayList<String>()
+        if(_schoolData?.board != null && !_schoolData.board.isNullOrBlank()){
+            items.add(_schoolData.board)
+        }
+
+        arrayOf("State Board", "CBSE", "ICSE", "NIOS", "IB", "CIE").forEach {
+            if(!items.any { itemName -> itemName.contains(it) }){
+                items.add(it)
+            }else{
+                binding.basicDetails.boardSpinner.setText(it)
+            }
+        }
+
         val adapter: ArrayAdapter<String> =
             ArrayAdapter<String>(
                 requireContext(),
@@ -248,8 +259,16 @@ class AddSchoolFragment : Fragment() {
             )
         dropdown.setAdapter(adapter)
 
+        //Labs dropdown set
+
         val labsDropdown: AutoCompleteTextView = binding.schoolDetails.existingLabs
-        val labsItems = arrayOf(
+        val labsItem = ArrayList<String>()
+
+        if(_schoolData?.existingLab != null && !_schoolData.existingLab.isNullOrBlank()){
+            labsItem.add(_schoolData.existingLab)
+        }
+
+        arrayOf(
             "Science Lab",
             "Computer Lab",
             "Engineering and Robotics Lab",
@@ -259,17 +278,59 @@ class AddSchoolFragment : Fragment() {
             "Physics and Electronics Lab",
             "Chemistry Lab",
             "Biology Lab"
-        )
+        ).forEach {
+            if(!labsItem.any { itemName -> itemName.contains(it) }){
+                labsItem.add(it)
+            }else{
+                binding.schoolDetails.existingLabs.setText(it)
+            }
+        }
         val labsAdapter: ArrayAdapter<String> =
             ArrayAdapter<String>(
                 requireContext(),
                 android.R.layout.simple_spinner_dropdown_item,
-                labsItems
+                labsItem
             )
         labsDropdown.setAdapter(labsAdapter)
+
+        //set lead type dropdown
+
+        val leadTypeDropdown: AutoCompleteTextView = binding.followupDetails.edtLeadType
+        val leadTypes = ArrayList<String>()
+
+        if(_schoolData?.leadType != null && !_schoolData.leadType.isNullOrBlank()){
+            leadTypes.add(_schoolData.leadType)
+        }
+
+        arrayOf(
+            "Hot",
+            "Cool",
+            "Warm",
+            "Dead",
+        ).forEach {
+            if(!leadTypes.any { itemName -> itemName.contains(it) }){
+                leadTypes.add(it)
+            }else{
+                binding.followupDetails.edtLeadType.setText(it)
+            }
+        }
+        val leadsAdapter: ArrayAdapter<String> =
+            ArrayAdapter<String>(
+                requireContext(),
+                android.R.layout.simple_spinner_dropdown_item,
+                leadTypes
+            )
+        leadTypeDropdown.setAdapter(leadsAdapter)
     }
 
     private fun observers() {
+        viewModel.newSchoolData.observe(viewLifecycleOwner){ _schoolData ->
+            setSchoolDetails()
+
+            setDropdowns(_schoolData)
+
+        }
+
         viewModel.addSchoolResponse.observe(viewLifecycleOwner) {
             Timber.e("Response dd")
             Timber.e(Gson().toJson(it))
@@ -327,7 +388,113 @@ class AddSchoolFragment : Fragment() {
 
         }
 
-        binding.followupDetails
+        binding.basicDetails.boardSpinner.setOnItemClickListener { parent, view, position, id ->
+            val updatedBoardName = parent.adapter.getItem(position) as String
+            viewModel._newSchoolData.value?.board = updatedBoardName
+        }
+
+        binding.schoolDetails.existingLabs.setOnItemClickListener { parent, view, position, id ->
+            val updatedLabName = parent.adapter.getItem(position) as String
+            viewModel._newSchoolData.value?.existingLab = updatedLabName
+        }
+
+        binding.followupDetails.edtLeadType.setOnItemClickListener { parent, view, position, id ->
+            val updatedLeadType = parent.adapter.getItem(position) as String
+            viewModel._newSchoolData.value?.leadType = updatedLeadType
+        }
+
+        binding.followupDetails.edtFollowType.setOnItemClickListener { parent, view, position, id ->
+            val updatedFollowUpLead = parent.adapter.getItem(position) as String
+            viewModel._newSchoolData.value?.nextFollowup = updatedFollowUpLead
+        }
+
+        binding.followupDetails.edtSchoolDate.setOnClickListener {
+            val c = Calendar.getInstance()
+            var year = c.get(Calendar.YEAR)
+            var month = c.get(Calendar.MONTH)
+            var day = c.get(Calendar.DAY_OF_MONTH)
+            if(!viewModel.newSchoolData.value?.nextFollowup.isNullOrEmpty()){
+                viewModel.newSchoolData.value?.nextFollowup?.split(" ")?.let {_dateAndTime ->
+                    binding.followupDetails.edtSchoolDate.setText(_dateAndTime[0])
+                    _dateAndTime[0].split("/").let {_dateArray ->
+                        day = _dateArray[0].toInt()
+                        month = _dateArray[1].toInt()
+                        year = _dateArray[2].toInt()
+                    }
+                }
+            }
+
+            val datePickerDialog = DatePickerDialog(
+                requireContext(),
+                { view, year, monthOfYear, dayOfMonth ->
+                    val updatedDateAndTime = dayOfMonth.toString() + "/" + (monthOfYear + 1) + "/" + year
+                    binding.followupDetails.edtSchoolDate.setText(updatedDateAndTime)
+                    viewModel.newSchoolData.value?.nextFollowup?.let{_nextFollowUpDate ->
+                        if(_nextFollowUpDate.contains(" ")){
+                            val dateAndTime = _nextFollowUpDate.split(" ")
+                            viewModel.newSchoolData.value?.nextFollowup = "$updatedDateAndTime ${dateAndTime[1]}"
+                        }else{
+                            viewModel.newSchoolData.value?.nextFollowup = updatedDateAndTime
+                        }
+
+                        Timber.e("Date")
+                        Timber.e(viewModel.newSchoolData.value?.nextFollowup)
+                    }
+                },
+                year,
+                month,
+                day
+            )
+
+            datePickerDialog.datePicker.minDate = Calendar.getInstance().apply {
+                this.set(Calendar.DAY_OF_MONTH, day)
+                this.set(Calendar.MONTH, month)
+                this.set(Calendar.YEAR, year)
+            }.timeInMillis- 1000
+            datePickerDialog.show()
+        }
+
+        binding.followupDetails.edtSchoolTime.setOnClickListener {
+            val c = Calendar.getInstance()
+
+            var hour = c.get(Calendar.HOUR_OF_DAY)
+            var minute = c.get(Calendar.MINUTE)
+            if(!viewModel.newSchoolData.value?.nextFollowup.isNullOrEmpty()){
+                viewModel.newSchoolData.value?.nextFollowup?.split(" ")?.let {_dateAndTime ->
+                    if(_dateAndTime.size >1) {
+                        binding.followupDetails.edtSchoolTime.setText(_dateAndTime[1])
+                        _dateAndTime[1].split(":").let { _timeArray ->
+                            hour = _timeArray[0].toInt()
+                            minute = _timeArray[1].toInt()
+                        }
+                    }
+                }
+            }
+
+            val timePickerDialog = TimePickerDialog(
+                requireContext(),
+                { view, hourOfDay, minute ->
+                    val updatedTime =  "$hourOfDay:$minute"
+                    binding.followupDetails.edtSchoolTime.setText(updatedTime)
+                    viewModel.newSchoolData.value?.nextFollowup?.let{_nextFollowUpDate ->
+                        if(_nextFollowUpDate.contains(" ")){
+                            val dateAndTime = _nextFollowUpDate.split(" ")
+                            viewModel.newSchoolData.value?.nextFollowup = "${dateAndTime[0]} $updatedTime"
+                        }else{
+                            viewModel.newSchoolData.value?.nextFollowup = "$_nextFollowUpDate $updatedTime"
+                        }
+
+                        Timber.e("Time")
+                        Timber.e(viewModel.newSchoolData.value?.nextFollowup)
+
+                    }
+                },
+                hour,
+                minute,
+                false
+            )
+            timePickerDialog.show()
+        }
     }
 
     private fun checkValidationsAndApiCall(stepCount: Int) {
@@ -397,8 +564,7 @@ class AddSchoolFragment : Fragment() {
             binding.basicDetails.tilBoard.error = null
 
         val isSchoolIntakeEmpty = binding.basicDetails.edtSchoolTotalIntake.text.toString()
-            .isNullOrEmpty() || binding.basicDetails.edtSchoolTotalIntake.text.toString()
-            .trim() == "0"
+            .isNullOrEmpty() || binding.basicDetails.edtSchoolTotalIntake.text.toString().trim() == "0"
         if (isSchoolIntakeEmpty)
             binding.basicDetails.tilSchoolTotalIntake.error =
                 resources.getString(R.string.please_enter_total_school_intake)
@@ -413,25 +579,25 @@ class AddSchoolFragment : Fragment() {
         else
             binding.basicDetails.tilCoordinatorName.error = null
 
-        val mPhonePattern = Pattern.compile("^[0123456789]{15}\$")
+        val mPhonePattern = Pattern.compile("[0123456789]{10,15}")
         val isCoordinatorPhoneValid =
             mPhonePattern.matcher(binding.basicDetails.edtCoordinatorMono.text.toString()).matches()
-        if (isCoordinatorPhoneValid)
+        if (!isCoordinatorPhoneValid)
             binding.basicDetails.tilCoordinatorMono.error =
                 resources.getString(R.string.please_enter_valid_phone_no)
         else
             binding.basicDetails.tilCoordinatorMono.error = null
 
         val isEmailValid =
-            Pattern.compile("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9]+[.-][a-zA-Z][a-z.A-Z]+\$").matcher(binding.basicDetails.edtEmailId.text.toString())
+            Pattern.compile("[a-zA-Z0-9+_.-]+@[a-zA-Z0-9]+[.-][a-zA-Z][a-z.A-Z]+").matcher(binding.basicDetails.edtEmailId.text.toString())
                 .matches()
-        if (isEmailValid)
+        if (!isEmailValid)
             binding.basicDetails.tilEmailId.error =
                 resources.getString(R.string.please_enter_valid_email_address)
         else
             binding.basicDetails.tilEmailId.error = null
 
-        return !(isSchoolNameEmpty || isSchoolAddressEmpty || isBoardEmpty || isSchoolIntakeEmpty || isCoordinatorNameEmpty || isCoordinatorNameEmpty || isCoordinatorPhoneValid || isEmailValid)
+        return !(isSchoolNameEmpty || isSchoolAddressEmpty || isBoardEmpty || isSchoolIntakeEmpty || isCoordinatorNameEmpty || isCoordinatorNameEmpty || !isCoordinatorPhoneValid || !isEmailValid)
     }
 
     private fun setPositionView() {
@@ -475,6 +641,13 @@ class AddSchoolFragment : Fragment() {
         binding.schoolDetails.edtAvgSchoolFee.setText(viewModel.newSchoolData.value?.avgSchoolFees)
         binding.schoolDetails.existingLabs.setText(viewModel.newSchoolData.value?.existingLab)
         binding.schoolDetails.edtValuePerStudent.setText(viewModel.newSchoolData.value?.expQuatedValue)
+        viewModel.newSchoolData.value?.nextFollowup.let {
+            val dateAndTime = it?.split(" ")
+            binding.followupDetails.edtSchoolDate.setText(
+                dateAndTime?.get(0))
+            if((dateAndTime?.size ?: 0) > 1)
+                binding.followupDetails.edtSchoolTime.setText(dateAndTime?.get(1) ?: "")
+        }
     }
 
     fun setButtonName(schoolData: SchoolData?) {
